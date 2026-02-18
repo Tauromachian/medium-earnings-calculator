2c5e0909c4dc9960554ec286d424b090e6e59a19
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-18 21:08:05 +0100

    Install and configure eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginPrettier from "eslint-plugin-astro";
+
+export default [
+  ...eslintPluginPrettier.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: [".astro/**", "dist/**", "node_modules/**"],
+  },
+  {
+    rules: {},
+  },
+];
